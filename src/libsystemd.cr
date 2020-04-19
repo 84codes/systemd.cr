0f36b76ddb5508ed0904d1ea37e5dfdb3ad43deb
Carl Hörberg <carl.hoerberg@gmail.com>
2020-04-19 07:55:25 +0200

    make the library no-op on non linux systems

--- src/libsystemd.cr
+++ src/libsystemd.cr
@@ -5,9 +5,11 @@ lib LibSystemD
   alias PidT = LibC::PidT
   alias UInt = LibC::UInt
   alias Char = UInt8
-  fun sd_listen_fds(unset_environment : Int) : Int
-  fun sd_listen_fds_with_names(unset_environment : Int, names : Char***) : Int
-  fun sd_notify(unset_environment : Int, state : Char*) : Int
-  fun sd_pid_notify(pid : PidT, unset_environment : Int, state : Char*) : Int
-  fun sd_pid_notify_with_fds(pid : PidT, unset_environment : Int, state : Char*, fds : Void*, n_fds : UInt) : Int
+  {% if flag?(:linux) %}
+    fun sd_listen_fds(unset_environment : Int) : Int
+    fun sd_listen_fds_with_names(unset_environment : Int, names : Char***) : Int
+    fun sd_notify(unset_environment : Int, state : Char*) : Int
+    fun sd_pid_notify(pid : PidT, unset_environment : Int, state : Char*) : Int
+    fun sd_pid_notify_with_fds(pid : PidT, unset_environment : Int, state : Char*, fds : Void*, n_fds : UInt) : Int
+  {% end %}
 end
--- src/systemd.cr
+++ src/systemd.cr
@@ -1,5 +1,8 @@
 require "./libsystemd"
 
+# Wrapper for libsystemd
+# http://man7.org/linux/man-pages/man3/sd_pid_notify_with_fds.3.html
+# http://man7.org/linux/man-pages/man3/sd_listen_fds_with_names.3.html
 class SystemD
   LISTEN_FDS_START = LibSystemD::SD_LISTEN_FDS_START
 
@@ -20,20 +23,29 @@ class SystemD
   end
 
   def self.notify(message = "READY=1\n")
-    LibSystemD.sd_notify(0, message).tap do |ret|
-      if ret == -1
-        raise Error.new
+    {% if flag?(:linux) %}
+      LibSystemD.sd_notify(0, message).tap do |ret|
+        if ret == -1
+          raise Error.new
+        end
       end
-    end
+    {% else %}
+      0
+    {% end %}
   end
 
   def self.listen_fds
-    fds = LibSystemD.sd_listen_fds(0)
-    raise Error.new if fds < 0
-    Array(Int32).new(fds) { |i| LISTEN_FDS_START + i }
+    {% if flag?(:linux) %}
+      fds = LibSystemD.sd_listen_fds(0)
+      raise Error.new if fds < 0
+      Array(Int32).new(fds) { |i| LISTEN_FDS_START + i }
+    {% else %}
+      Array(Int32).new(0)
+    {% end %}
   end
 
   def self.listen_fds_with_names
+    {% if flag?(:linux) %}
     val = Pointer(UInt8).null
     arr = pointerof(val)
     fds = LibSystemD.sd_listen_fds_with_names(0, pointerof(arr))
@@ -46,26 +58,35 @@ class SystemD
     end
     LibC.free arr
     names
+    {% else %}
+      Array(Tuple(Int32, String)).new(0)
+    {% end %}
   end
 
   def self.store_fds(fds : Array(Int32))
-    LibSystemD.sd_pid_notify_with_fds(0, 0, "FDSTORE=1\n",
-                                      fds.to_unsafe, fds.size).tap do |ret|
- 
-      if ret < 0
-        raise Error.new
+    {% if flag?(:linux) %}
+      LibSystemD.sd_pid_notify_with_fds(0, 0, "FDSTORE=1\n",
+                                        fds.to_unsafe, fds.size).tap do |ret|
+
+        if ret < 0
+          raise Error.new
+        end
       end
-    end
+    {% else %}
+      0
+    {% end %}
   end
 
   def self.store_fds(fds : Array(Int32), fd_name : String)
-    LibSystemD.sd_pid_notify_with_fds(0, 0, "FDSTORE=1\nFDNAME=#{fd_name}\n",
-                                      fds.to_unsafe, fds.size).tap do |ret|
- 
-      if ret < 0
-        raise Error.new
-      end
-    end
+    {% if flag?(:linux) %}
+      res = LibSystemD.sd_pid_notify_with_fds(0, 0,
+                                              "FDSTORE=1\nFDNAME=#{fd_name}\n",
+                                              fds.to_unsafe, fds.size)
+      raise Error.new if res < 0
+      res
+    {% else %}
+      0
+    {% end %}
   end
 
   def self.remove_fds(fd_name : String)
