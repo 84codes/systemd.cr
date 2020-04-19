d3b9986c6de9dbc24ce3b47945010c529e9a590c
Carl Hörberg <carl.hoerberg@gmail.com>
2020-04-19 07:40:40 +0200

    More specs

--- spec/spec_helper.cr
+++ spec/spec_helper.cr
@@ -1,17 +1,5 @@
 require "spec"
 require "socket"
 require "../src/systemd"
-ENV["NOTIFY_SOCKET"] = path = "/tmp/systemd_cr_spec.sock"
-sock = Socket.unix(Socket::Type::DGRAM)
-sock.bind Socket::UNIXAddress.new(path)
-spawn do
-  loop do
-    response, addr = sock.receive
-    p response, addr
-  rescue
-    next
-  end
-ensure
-  sock.close
-  File.delete path
-end
+
+ENV["LISTEN_PID"] = Process.pid.to_s
--- spec/systemd_spec.cr
+++ spec/systemd_spec.cr
@@ -2,6 +2,22 @@ require "./spec_helper"
 
 describe SystemD do
   it "can notify" do
+    ENV["NOTIFY_SOCKET"] = path = File.tempname
+    sock = Socket.unix(Socket::Type::DGRAM)
+    sock.bind Socket::UNIXAddress.new(path)
     SystemD.notify_ready.should eq 1
+    sock.close
+    File.delete path
+  end
+
+  it "can get listen fds" do
+    ENV["LISTEN_FDS"] = "2"
+    SystemD.listen_fds.should eq [3, 4]
+  end
+
+  it "can get listen fds with names" do
+    ENV["LISTEN_FDS"] = "2"
+    ENV["LISTEN_FDNAMES"] = "echo.socket:stored"
+    SystemD.listen_fds_with_names.should eq [{ 3, "echo.socket" }, { 4, "stored" }]
   end
 end
