d39f3ebf6ac37e1871b5cd3bc3e6b428149b05b7
Carl Hörberg <carl@84codes.com>
2021-06-29 11:50:15 +0200

    without_systemd in libsystemd

--- src/libsystemd.cr
+++ src/libsystemd.cr
@@ -5,7 +5,7 @@ lib LibSystemD
   alias PidT = LibC::PidT
   alias UInt = LibC::UInt
   alias Char = UInt8
-  {% if flag?(:linux) %}
+  {% if flag?(:linux) && !flag?(:without_systemd) %}
     fun sd_listen_fds(unset_environment : Int) : Int
     fun sd_listen_fds_with_names(unset_environment : Int, names : Char***) : Int
     fun sd_notify(unset_environment : Int, state : Char*) : Int
